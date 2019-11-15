1ccd608cd532b6184513087189ebdb299cc76a4d
Volodymyr Syvoraksha <vpsyvoraksha@gmail.com>
2019-11-16 01:26:05 +0200

    prepear app dor deployment

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
-
-
+const path = require('path')
 // Connect DB
 connectDB();
 
@@ -10,14 +9,17 @@ const app = express();
 
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) => res.json({msg: 'Welcome to the Lebe.dev interview'}));
-
 // Define routes
-app.use('/api/users', require('./routes/users'))
-app.use('/api/auth', require('./routes/auth'))
-app.use('/api/contacts', require('./routes/contacts'))
+app.use('/api/users', require('./routes/users'));
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/contacts', require('./routes/contacts'));
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')))
+}
 
 const PORT = process.env.PORT || 5000;
-
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
